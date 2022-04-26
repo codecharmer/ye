40eda54a2ed275b7a15d691e056ae0b962a0c31c
Alfredo Prince <alfredo-j.prince@capgemini.com>
2022-04-26 14:32:37 -0500

    Getting the thumbnail URL

--- themes/ye/header.php
+++ themes/ye/header.php
@@ -70,14 +70,13 @@
 			</div><!-- /.container -->
 		</nav><!-- /#header -->
 
-		<div class="p-5 text-center bg-image" style="background-image: url('https://mdbcdn.b-cdn.net/img/new/slides/041.webp'); height: 400px">
+		<div class="p-5 text-center bg-image" style="background-image: url('<?php  echo get_the_post_thumbnail_url( get_the_id() );?>'); height: 400px">
 			<div class="mask" style="background-color: rgba(0, 0, 0, 0.6);">
 				<div class="d-flex justify-content-center align-items-center h-100">
 					<div class="text-white">
-					<h1 class="mb-3">Heading</h1>
-					<h4 class="mb-3">Subheading</h4>
+					<h1 class="mb-3"><?= the_title();?></h1>
 					<a class="btn btn-outline-light btn-lg" href="#!" role="button"
-					>Call to action</a
+					>Buy the new Album</a
 					>
 					</div>
 				</div>
